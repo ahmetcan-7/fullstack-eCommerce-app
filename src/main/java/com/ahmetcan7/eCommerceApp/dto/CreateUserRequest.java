ff3c800e0ea5fb0cd94445403c5dc9bd15fdd159
ahmetcan7 <ahmetcan423507@gmail.com>
2021-10-14 13:43:28 +0300

    added regex

--- src/main/java/com/ahmetcan7/eCommerceApp/dto/CreateUserRequest.java
+++ src/main/java/com/ahmetcan7/eCommerceApp/dto/CreateUserRequest.java
@@ -6,20 +6,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateUserRequest {
-    @NotBlank
+    @NotNull
+    @Size(min = 3,max = 25)
     @UniqueUserName
     private String userName;
 
-    @NotBlank
+    @NotNull
+    @Size(min = 3,max = 25)
     private String displayName;
 
-    @NotBlank
+    @NotNull
+    @Size(min = 8, max=25)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 }
